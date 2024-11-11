1f21d6c11cacb6ed08f305fddfc53eda6e3adb20
Nithin <10778861+darthnithin@users.noreply.github.com>
2024-11-11 01:28:59 -0800

    get rid of old stuff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
 import pandas as pd
-import networkx as nx
-import matplotlib.pyplot as plt
 
 from flask import Flask, render_template_string
 from flask_caching import Cache
@@ -32,38 +30,7 @@ IMPORTANT_LANGUAGE_LIST = [
                         "Late Latin",
                     ]
 
-def plot_word_graph(df, word):
-    # filter
-    sub_df = df[(df["term"] == word) | (df["related_term"] == word)]
 
-    G = nx.DiGraph()
-
-    for _, row in sub_df.iterrows():
-        if row["term"] == word:
-            G.add_edge(row["term"], row["related_term"], relationship=row["reltype"])
-        else:
-            G.add_edge(row["related_term"], row["term"], relationship=row["reltype"])
-
-    plt.figure(figsize=(10, 6))
-    pos = nx.spring_layout(G)
-    nx.draw(
-        G,
-        pos,
-        with_labels=True,
-        node_size=3000,
-        node_color="lightblue",
-        font_size=10,
-        font_weight="bold",
-        arrows=True,
-    )
-
-    # Draw edge labels for relationships
-    edge_labels = {(u, v): d["relationship"] for u, v, d in G.edges(data=True)}
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color="red")
-
-    # Show the plot
-    plt.title(f"Graph of '{word}' and Related Terms")
-    plt.show()
 
 
 @app.route('/words/<path:words>')
@@ -177,95 +144,7 @@ def clear_cache():
     print("Cleared Cache!")
     return "Cache Cleared!"
 
-def add_word(df, word, net, searched_words):
-    sub_df = df[((df["term"] == word) | (df["related_term"] == word))]
-    for _, row in sub_df.iterrows():
-        # Add nodes and edges
 
-        if (net.num_nodes() < 100) and (
-            (row["term"], row["related_term"]) not in searched_words
-        ):
-            # print(row)
-            if pd.notna(row["related_term"]):
-                # if term is not english and has an arrow leaving, color red
-                if (row["lang"] != "English") and (
-                    not (
-                        (str(row["reltype"]).split("_")[1] == "of")
-                        or (str(row["reltype"].split("_")[0] == "has"))
-                    )
-                ):
-                    net.add_node(
-                        row["term"],
-                        label=row["term"],
-                        title=f"{row['lang']}\n{str(row['position'])}\n{str(row['group_tag'])}",
-                        color="red",
-                    )
-                else:
-                    net.add_node(
-                        row["term"],
-                        label=row["term"],
-                        title=f"{row['lang']}\n{str(row['position'])}\n{str(row['group_tag'])}",
-                    )
-                if (row["related_lang"] != "English") and (
-                    (
-                        (str(row["reltype"]).split("_")[1] == "of")
-                        or (str(row["reltype"].split("_")[0] == "has"))
-                    )
-                ):
-                    net.add_node(
-                        row["related_term"],
-                        label=row["related_term"],
-                        title=row["related_lang"],
-                        color="red",
-                    )
-                else:
-                    net.add_node(
-                        row["related_term"],
-                        label=row["related_term"],
-                        title=row["related_lang"],
-                    )
-                if (str(row["reltype"]).split("_")[1] == "of") or (
-                    str(row["reltype"].split("_")[0] == "has")
-                ):
-                    net.add_edge(
-                        row["related_term"],
-                        row["term"],
-                        title=row["reltype"],
-                        arrows="to",
-                    )
-                else:
-                    net.add_edge(
-                        row["term"],
-                        row["related_term"],
-                        title=row["reltype"],
-                        arrows="to",
-                    )
-                searched_words.append((row["term"], row["related_term"]))
-                if row["lang"] != "English":
-                    add_word(df, row["term"], net, searched_words)
-    # print(net.num_nodes(), len(searched_words))
-
-
-def search_word(df):
-    while True:
-        word = input("Enter a word to search (or type 'quit' to exit): ").strip()
-        if word.lower() == "quit":
-            print("Exiting the search.")
-            break
-
-        # Search for the word in both the 'term' and 'related_term' columns
-        result = df[(df["term"] == word) | (df["related_term"] == word)]
-
-        if not result.empty:
-            print("Results found:")
-            for i, row in result.iterrows():
-                print(i, ": ", row)
-            selection = int(input("Select an index: ").strip())
-            print(selection)
-            selected_word = result.loc[selection]
-            print(selected_word)
-        else:
-            print(f"No results found for '{word}'. Please try another word.")
 
 
 
