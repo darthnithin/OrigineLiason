e7b83a8fd20f49aeb54aa409eeea076da0ff97cf
Nithin <10778861+darthnithin@users.noreply.github.com>
2024-11-10 23:16:53 -0800

    implement BFS

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 from pyvis.network import Network
+from collections import deque
 
 from data_utils import generate_filtered_data
 
 CULLED_FILE_PATH = "filtered_etymology_db.csv.gz"
 
+IMPORTANT_LANGUAGE_LIST = [
+                        "French",
+                        "Latin",
+                        "Ancient Greek",
+                        "Old French",
+                        "Medieval Latin",
+                        "Late Latin",
+                    ]
 
 def plot_word_graph(df, word):
     # filter
@@ -54,8 +63,9 @@ def plot_word_graph_pyvis(df, word):
         select_menu=True,
         filter_menu=True,
     )
-    searched = []
-    add_word(df, word, net, searched)
+    searched = set()
+    #add_word(df, word, net, searched)
+    add_word_bfs(df, word, net, searched)
 
     net.show_buttons()
     html_content = net.generate_html()
@@ -63,6 +73,69 @@ def plot_word_graph_pyvis(df, word):
         f.write(html_content)
     # net.show("word_graph.html")
     print("done")
+
+def add_word_bfs(df, word, net, searched_words):
+    queue = deque([word])
+    
+
+    while queue and net.num_nodes() < 125:
+        current_word = queue.popleft()
+        if current_word in searched_words:
+            continue
+        print(current_word)
+        sub_df = df[(df["term"] == current_word) | (df["related_term"] == current_word)]
+        for _, row in sub_df.iterrows():
+
+            term, related_term = str(row["term"]), str(row["related_term"])
+            relationship = str(row["reltype"])
+
+            if "of" in relationship or "from" in relationship or relationship.startswith("has"):
+                edge_start, edge_end = related_term, term
+            else:
+                #edge_start, edge_end = word_to_add, related_to_add
+                edge_start, edge_end = term, related_term
+            
+            term_color_condition = (row["lang"] != "English" and edge_start == term)
+            related_term_color_condition = (row["related_lang"] != "English" and edge_start == related_term)
+
+            if term not in searched_words:
+                net.add_node(
+                    term,
+                    label=term,
+                    title=str(row["lang"]),
+                    color="red" if term_color_condition else "blue",
+                )
+            if related_term not in searched_words:
+                net.add_node(
+                    related_term,
+                    label=related_term,
+                    title=str(row["related_lang"]),
+                    color="red" if related_term_color_condition else "blue",
+                )
+            if relationship == "etymologically_related_to":
+                net.add_edge(edge_start, edge_end, title=relationship)
+            elif relationship not in ["group_derived_root", "cognate_of"]:
+                net.add_edge(edge_start, edge_end, title=relationship, arrows="to")
+                
+            
+            if (term not in queue) and (term not in searched_words):
+                print(term)
+                if str(row['lang']) in IMPORTANT_LANGUAGE_LIST and term == edge_start:
+                #if str(row['related_lang']) in IMPORTANT_LANGUAGE_LIST:
+                    queue.appendleft(term)
+                else:
+                    queue.append(term)
+
+            if (related_term not in queue) and (related_term not in searched_words):
+                #if str(row['related_lang']) in IMPORTANT_LANGUAGE_LIST and related_term == edge_start:
+                print(related_term)
+
+                if str(row['related_lang']) in IMPORTANT_LANGUAGE_LIST and related_term == edge_start:
+                    #print(searched_words)
+                    queue.appendleft(related_term)
+                else:
+                    queue.append(related_term)
+        searched_words.add(current_word)
 
 
 def add_word(df, word, net, searched_words):
@@ -129,7 +202,8 @@ def add_word(df, word, net, searched_words):
                         arrows="to",
                     )
                 searched_words.append((row["term"], row["related_term"]))
-                add_word(df, row["term"], net, searched_words)
+                if row["lang"] != "English":
+                    add_word(df, row["term"], net, searched_words)
     # print(net.num_nodes(), len(searched_words))
 
 
@@ -164,7 +238,7 @@ def main():
     df = pd.read_csv(CULLED_FILE_PATH, compression="gzip")
     # search_word(df)
     # plot_word_graph(df, "donation")
-    plot_word_graph_pyvis(df, "father")
+    plot_word_graph_pyvis(df, "text")
 
 
 # Only run main if this script is executed directly
